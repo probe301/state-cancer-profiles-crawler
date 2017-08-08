2b8ed93a03a36efef750f11ab903dd980e10e8fc
Probe <probe301@gmail.com>
2017-08-08 18:08:36 +0800

    done fetch epa

--- epa_crawler.py
+++ epa_crawler.py
@@ -1,101 +1,23 @@
 
-
-import shutil
-
+from pyquery import PyQuery
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import FirefoxProfile
+from selenium.webdriver.support.ui import Select
+import os
+import time
 import csv
 
-
-# ==============================
-# ==============================
-# common
-# ==============================
-# ==============================
-
-# import os
-# import sys
-# import re
-# import time
-# import random
-# import traceback
-# import sein
-# from sein import String as ss
-
-# import io
-# import os
-# import sys
-# from pprint import pprint
-# import re
-# from time import sleep
-# from pylon import all_files
-# from pylon import datalines
-# from pylon import windows
-# from pylon import enumrange
-# import platform
-
-# import random
-# import math
-# import fnmatch
-# import inspect
-# import datetime
-# import itertools
-# from itertools import groupby
-
-# from collections import OrderedDict
-# from collections import defaultdict
-# from collections import namedtuple
-# from collections import Counter
-# from collections import deque
-
-# from pprint import pprint
-
-# from pylon import dedupe
-# from pylon import rotate
-# from pylon import flatten
-# from pylon import transpose
-# from pylon import rand
-# from pylon import all_files
-# from pylon import datalines
-# from pylon import datamatrix
-# from pylon import file_timer
-# from pylon import windows
-# from pylon import transpose
-# from pylon import random_split
-# from pylon import load_title
-# from pylon import load_csv
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ==============================
-# ==============================
-# crawler
-# ==============================
-# ==============================
-
-# import html2text
-# import requests
-# sess = requests.Session()
-# from pyquery import PyQuery
-# from selenium import webdriver
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-# from selenium.common.exceptions import TimeoutException, NoSuchElementException
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver import FirefoxProfile
-
-
-
+def record_csv(headers, content, path):
+  with open(path, 'w', encoding='utf-8') as f:
+    f_csv = csv.writer(f, lineterminator='\n')
+    f_csv.writerow(headers)
+    f_csv.writerows(content)
+  print('save csv done', path)
 
 
 
@@ -117,132 +39,91 @@ session = requests.Session()
 
 
 
-
-
-state_ids = '''
-
-'''
-
-
-
-
-
-
-def test_requests_with_cookie():
-  cookie_text = '''
-
-    Host: ofmpub.epa.gov
-    Connection: keep-alive
-    Upgrade-Insecure-Requests: 1
-    User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36
-    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
-    Referer: https://ofmpub.epa.gov/apex/sfdw/f?p=108:103:::NO:RP::
-    Accept-Encoding: gzip, deflate, br
-    Accept-Language: zh-CN,zh;q=0.8
-    Cookie: ORA_WWV_APP_108=ORA_WWV-lkCV6C-ZDyEH-33g7wGsz6yA; _ceg.s=otyph1; _ceg.u=otyph1; has_js=1; _ga=GA1.2.1107459310.1501516229; _gid=GA1.2.2014277172.1502115954; acs.t=%7B%22_ckX%22%3A1509292394728%2C%22rid%22%3A%22de35431-94229866-5529-4d1f-2e785%22%2C%22cp%22%3A%7B%22url%22%3A%22https%3A%2F%2Fofmpub.epa.gov%2Fapex%2Fsfdw%2Ff%3Fp%3D108%3A103%3A%3A%3ANO%3ARP%3A%3A%22%2C%22VisitorID%22%3A%22%22%2C%22section_www2%22%3A%22N%22%2C%22section_www%22%3A%22Y%22%2C%22nlquery%22%3A%22N%22%2C%22GA_UID%22%3A%221107459310.1501516229%22%2C%22terms%22%3A%22%22%2C%22browser%22%3A%22Chrome%2059%22%2C%22os%22%3A%22Mac%22%2C%22flash%22%3A%2226.0%22%2C%22hosted%22%3A%22true%22%2C%22referrer%22%3A%22%22%2C%22site%22%3A%22epa.gov%22%2C%22trigger_version%22%3A%2219.0.33%22%2C%22pv%22%3A%224%22%2C%22locale%22%3A%22en%22%2C%22dn%22%3A%22default%22%7D%2C%22pl%22%3A1%2C%22pv%22%3A4%2C%22def%22%3A0%2C%22browsepv%22%3A4%2C%22dn%22%3A%22default%22%2C%22i%22%3A%22d%22%7D
-
+class DriverFindElementByCSS:
+  ''' usage:
+      driver / css(pat)  => one element
+      driver // css(pat)  => [element list]
   '''
+  def __call__(self, pat):
+    self.pat = pat
+    return self
+  def __rtruediv__(self, driver):
+    return driver.find_element_by_css_selector(self.pat)
+  def __rfloordiv__(self, driver):
+    return driver.find_elements_by_css_selector(self.pat)
 
-  cookie_text = '''
-   ORA_WWV_APP_108=ORA_WWV-lkCV6C-ZDyEH-33g7wGsz6yA;
-   _ceg.s=otyph1;
-   _ceg.u=otyph1;
-   has_js=1;
-   _ga=GA1.2.1107459310.1501516229;
-   _gid=GA1.2.2014277172.1502115954;
-   acs.t={"_ckX":1509292394728,"rid":"de35431-94229866-5529-4d1f-2e785","cp":{"url":"https://ofmpub.epa.gov/apex/sfdw/f?p=108:103:::NO:RP::","VisitorID":"","section_www2":"N","section_www":"Y","nlquery":"N","GA_UID":"1107459310.1501516229","terms":"","browser":"Chrome 59","os":"Mac","flash":"26.0","hosted":"true","referrer":"","site":"epa.gov","trigger_version":"19.0.33","pv":"4","locale":"en","dn":"default"},"pl":1,"pv":4,"def":0,"browsepv":4,"dn":"default","i":"d"}
-  '''
-
+css = DriverFindElementByCSS()
 
 
 
+def get_windows_firefox_driver():
+  gecko_path = '/Users/probe/git/Lottery_Hedge_Crawler/geckodriver'
+  gecko_path = 'C:/Program Files/Mozilla Firefox/geckodriver.exe'
+  profile = webdriver.FirefoxProfile()
+  # profile.set_preference("browser.download.dir","/Users/probe/git/StateCancerProfilesCrawler/");
+  # profile.setPreference("browser.download.folderList", 2);
+  profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream,text/csv");
+  profile.set_preference("browser.download.dir",os.getcwd())
 
-  # cookies = {line.split(':')[0]: line.split(':')[1].strip() for line in datalines(cookie_text)}
-  # print(cookies)
-
-  url = 'https://ofmpub.epa.gov/apex/sfdw/f?p=108:103::CSV::::'
-
-  response = session.get(url, cookies=cookie_text)
-  print(response.text)
-
-
-
-
-
+  # if proxies:
+  #   profile.set_preference("network.proxy.type", 1)
+  #   profile.set_preference("network.proxy.socks", "127.0.0.1")
+  #   profile.set_preference("network.proxy.socks_port", 1080)
+  #   profile.set_preference("network.proxy.socks_version", 5)
+  #   # profile.setAssumeUntrustedCertificateIssuer(False)
+  #   profile.update_preferences()
+  driver = webdriver.Firefox(firefox_profile=profile,
+                             executable_path=gecko_path)
+  return driver
 
 
-# https://ofmpub.epa.gov/apex/sfdw/f?p=108:103:::NO:RP::
-# <a href="f?p=108:103::CSV::::" id="SFDWIR_download_CSV">
-#   <img src="/i_owpub/ws/download_csv_64x64.gif" alt="CSV" title="CSV">
-# </a>
 
-# water_system_summary_{state_code}
+def fetch_one_page(driver, state_code):
+  driver.get('https://ofmpub.epa.gov/apex/sfdw/f?p=108:200::::::')
+  print('get US map page...')
+#   state_code = '09'
+  selector = Select(driver / css('#P200_STATE'))
+  selector.select_by_value(state_code)
+  search_button = driver / css('#P200_SEARCH')
+  search_button.click()
+  print('click search button...')
+  WebDriverWait(driver, 120).until(
+      EC.presence_of_element_located((By.CSS_SELECTOR, '#SFDWIR_row_select'))
+  )
+  change_all_selector = Select(driver / css('#SFDWIR_row_select'))
+  change_all_selector.select_by_value('100000')
+  print('click show all rows...')
+  WebDriverWait(driver, 120).until(
+      EC.invisibility_of_element_located((By.CSS_SELECTOR, 'span.u-Processing-spinner'))
+  )
+  WebDriverWait(driver, 400).until(
+      EC.presence_of_element_located((By.CSS_SELECTOR, '.t-fht-tbody'))
+  )
+
+  time.sleep(10)
+  print('prepare to fetch')
+  tables_header, tables_body = driver // css('.a-IRR-table')
+  headers = [td.text.replace('\n', ' ') for td in tables_header // css('th')]
+
+
+  content = []
+  for row in PyQuery(tables_body.get_attribute('outerHTML'))('tr'):
+    row = PyQuery(row)
+    if not row:
+      continue
+    row = [td.text for td in row('td')]
+    if row and row[0]:
+      content.append(row)
+  record_csv(headers, content, 'epa_water_system_summary_{}_[{}].csv'.format(state_code, len(content)))
+
+
 state_codes = '''
-AK: AK
-AL: AL
-AR: AR
-AZ: AZ
-CA: CA
-CO: CO
-CT: CT
-DC: DC
-DE: DE
-FL: FL
-GA: GA
-HI: HI
-IA: IA
-ID: ID
-IL: IL
-IN: IN
-KS: KS
-KY: KY
-LA: LA
-MA: MA
-MD: MD
-ME: ME
-MI: MI
-MN: MN
-MO: MO
-MS: MS
-MT: MT
-NC: NC
-ND: ND
-NE: NE
-NH: NH
-NJ: NJ
-NM: NM
-NV: NV
-NY: NY
-OH: OH
-OK: OK
-OR: OR
-PA: PA
-RI: RI
-SC: SC
-SD: SD
-TN: TN
-TX: TX
-UT: UT
-VA: VA
-VT: VT
-WA: WA
-WI: WI
-WV: WV
-WY: WY
-AS: AS (Territory)
-GU: GU (Territory)
-MP: MP (Territory)
-PR: PR (Territory)
-VI: VI (Territory)
-01: 01 (Tribe)
-02: 02 (Tribe)
-03: 03 (Tribe)
-04: 04 (Tribe)
-05: 05 (Tribe)
-06: 06 (Tribe)
-07: 07 (Tribe)
-08: 08 (Tribe)
-09: 09 (Tribe)
-10: 10 (Tribe)
-NN: NN (Tribe)
+  AK AL AR AZ CA CO CT DC DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY AS MP PR VI 01 02 03 04 05 06 07 08 09 10 NN
 '''
+state_codes = state_codes.strip().split(' ')
+
+
+driver = get_windows_firefox_driver()
+
+for state_code in state_codes[:3]:
+  fetch_one_page(driver=driver, state_code=state_code)
